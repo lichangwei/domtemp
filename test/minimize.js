3d1951feadcbc09916fbc1c56353f2a924bd6138
lichangwei <lichangwei@love.com>
2012-10-31 15:17:34 +0800

    add the compressed version

---
+++ test/minimize.js
@@ -0,0 +1,12 @@
+
+require('webtools')
+    .uglifyjs({
+        'input': [
+            '../domtemp.js',
+            '../attr-scanner.js',
+            '../text-scanner.js',
+            '../form-scanner.js',
+            '../loop-scanner.js',
+        ],
+        'output': 'domtemp-min.js'
+    })
