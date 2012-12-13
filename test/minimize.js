62f6f87fdc38b05a2066de9571638bbbb165a589
lichangwei <lichangwei@love.com>
2012-12-13 18:39:44 +0800

    *

--- test/minimize.js
+++ test/minimize.js
@@ -1,12 +1,12 @@
 
 require('webtools')
-    .uglifyjs({
-        'input': [
-            '../domtemp.js',
-            '../attr-scanner.js',
-            '../text-scanner.js',
-            '../form-scanner.js',
-            '../loop-scanner.js',
-        ],
-        'output': 'domtemp-min.js'
-    })
+  .uglifyjs({
+    'input': [
+      '../domtemp.js',
+      '../attr-scanner.js',
+      '../text-scanner.js',
+      '../form-scanner.js',
+      '../loop-scanner.js',
+    ],
+    'output': 'domtemp-min.js'
+  });
